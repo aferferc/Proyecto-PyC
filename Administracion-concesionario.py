bda3a3cebb933f546aa14712f6a1d508684250dc
PabloJose2 <pgonmaq0807@g.educaand.es>
2024-12-12 16:55:32 +0000

    E arregrado un par de erores en el codigo de la funcion main, e añadido una comprobacion para que main se ejecute al iniciar el programa y e creado el objeto veiculo con su constructor conteniendo sus respectivos atrivutos y una funcion descripcion para usarla mas adelante para dar la descripcion de cada objeto en el inventario

--- Administracion-concesionario.py
+++ Administracion-concesionario.py
@@ -1,6 +1,22 @@
-def main() #crea la funcion mai, que es la que estara ligada al menu
-    entrada = 78 #crea la variable entrada y le asigna un valor entero aleatorio, este valor no es relevante
-    while entrada != "": #inicia un bucle con while en el que ejecutara el programa en el interior asta que la variable entrada este vacia, cosa que no pasa en ningun momento por lo que el bucle continuara asta que se seleccione la obcion 4, que lo detendra con un break
+class veiculo(): #creacion de la clase para el producto, en este caso un veiculo
+    def __init__(self, color, potencia, tipo_carroceria, eje_traccion, precio, marca, modelo): #creacion del constructor asignando las variables externas que contienen los atributos
+        self.color = color #creacion del atributo color a traves de la variable externa color
+        self.potencia = potencia #creacion del atributo potencia a traves de la variable externa potencia
+        self.tipo_carroceria = tipo_carroceria #creacion del tipo_carroceria color a traves de la variable externa tipo_carroceria
+        self.eje_traccion = eje_traccion #creacion del atributo eje_traccion a traves de la variable externa eje_traccion
+        self.precio = precio #creacion del atributo precio a traves de la variable externa precio
+        self.marca = marca #creacion del atributo marca a traves de la variable externa marca
+        self.modelo = modelo #creacion del atributo modelo a traves de la variable externa modelo
+
+    def.descripcion(self):
+        return f"Especificaciones: {self.marca} {self.modelo}, Carroceria: {self.tipo_carroceria}, Color: {self.color}, Potencia: {self.potencia}, Traccion: {self.eje_traccion}"
+
+inventario = [] #crea la variable inventario asignandole una lista vacia
+
+
+
+def main(): #crea la funcion mai, que es la que estara ligada al menu
+    while True: #Inicia un bucle infinitro asignando un valor buleano
         print("¿Que quiere hacer?(Introduzca el numero de la acción deseada.)") #le dice al usuario cuales son las obciones del menu
         print("1.Agregar un coche al almacen")
         print("2.Modificar un coche del almacen")
@@ -20,3 +36,5 @@ def main() #crea la funcion mai, que es la que estara ligada al menu
             break
         else: #llegados a este punto, si el valor no a coincidido con ninguna obcion se le pide al usuario antes de que se reinicie el bucle que introduzca un valo valido
             print("entrada incorrecta, vuelvelo a intentar")
+if __name__ == "__main__": #comprueba si el nombre de la funcion es main, si es asi llama la funcion main, esto es solo para llamar a la funcion main cuando inicia el programa
+    main()
